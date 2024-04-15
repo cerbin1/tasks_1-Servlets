9d8de390d5e46fbc9ed1f3aa7c7c056d2f9f9ffa
cerbin1 <gekarez@gmail.com>
2024-04-15 13:01:50 +0200

    Add chart displaying number of tasks per day in stats page

--- src/Stats.java
+++ src/Stats.java
@@ -7,10 +7,13 @@ import service.EmailSendingService;
 import service.StatisticService;
 import service.UserService;
 import service.dto.StatisticDto;
+import service.dto.TasksCountForDateDto;
 import utils.UiUtils;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
 
 public class Stats extends HttpServlet {
 
@@ -27,13 +30,47 @@ public class Stats extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         if (authenticationService.authenticate(request)) {
             PrintWriter writer = response.getWriter();
-            StatisticDto statistics = statisticService.getStatistics();
+            StatisticDto statistics = statisticService.getGeneralStatistics();
+
+
+            List<TasksCountForDateDto> numberOfTasks = statisticService.getNumberOfTasks();
+            StringBuilder dateValues = new StringBuilder("[");
+            numberOfTasks.forEach(tasksCountForDateDto -> dateValues.append("\"").append(tasksCountForDateDto.getDate().toString()).append("\","));
+            dateValues.append("]");
+            String taskCounts = Arrays.toString(numberOfTasks.stream().map(TasksCountForDateDto::getCount).toArray());
             writer.print("<html lang=\"en\">" +
                     "<head>" +
                     "    <title></title>" +
                     "    <script src=\"bootstrap.bundle.min.js\"></script>" +
+                    "    <script src=\"https://cdn.jsdelivr.net/npm/chart.js\"></script>" +
+                    "<script src=\"https://cdn.jsdelivr.net/npm/chartjs-adapter-date-fns/dist/chartjs-adapter-date-fns.bundle.min.js\"></script>\n" +
                     "    <link rel=\"stylesheet\" href=\"bootstrap.min.css\">" +
                     "</head>" +
+                    "<script>" +
+                    "window.onload = (event) => {" +
+                    "    const ctx = document.getElementById('numberOfTasksChart');" +
+                    "    if (window.numberOfTasksChart && typeof window.numberOfTasksChart.destroy === 'function') {" +
+                    "      window.numberOfTasksChart.destroy();" +
+                    "    }" +
+                    "    window.numberOfTasksChart = new Chart(ctx, {" +
+                    "      type: 'bar'," +
+                    "      data: {" +
+                    "        labels: " + dateValues + "," +
+                    "        datasets: [{" +
+                    "          label: '# of tasks'," +
+                    "          data: " + taskCounts + "," +
+                    "          borderWidth: 1" +
+                    "        }]" +
+                    "      }," +
+                    "      options: {" +
+                    "        scales: {" +
+                    "        x: {type: 'time', time: {tooltipFormat: 'yyyy-MM-dd', unit: 'day',}, title: {display: true, text: 'Date'}}," +
+                    "          y: {beginAtZero: true, title: {display: true, text: 'Count'}}" +
+                    "        }" +
+                    "      }" +
+                    "    });" +
+                    "  }" +
+                    "</script>" +
                     "<body>" +
                     UiUtils.navbarHtml() +
                     "<h2>General statistics</h2>" +
@@ -67,6 +104,8 @@ public class Stats extends HttpServlet {
                     "          <span className=\"fw-bold px-1\">" + statistics.getNumberOfNotifications() + "</span>" +
                     "        </div>" +
                     "      </div>" +
+                    "    <h2>Number of tasks per day by deadline</h2>" +
+                    "    <canvas id=\"numberOfTasksChart\"></canvas>" +
                     "</body>" +
                     "</html>");
         } else {
--- src/service/StatisticService.java
+++ src/service/StatisticService.java
@@ -5,6 +5,14 @@ import db.dao.SubtaskDao;
 import db.dao.TaskDao;
 import db.dao.UserDao;
 import service.dto.StatisticDto;
+import service.dto.TasksCountForDateDto;
+
+import java.time.LocalDate;
+import java.util.*;
+import java.util.Map.Entry;
+
+import static java.util.Map.Entry.comparingByKey;
+import static java.util.stream.Collectors.*;
 
 public class StatisticService {
 
@@ -20,12 +28,37 @@ public class StatisticService {
         this.notificationDao = notificationDao;
     }
 
-    public StatisticDto getStatistics() {
+    public StatisticDto getGeneralStatistics() {
         return new StatisticDto(userDao.getNumberOfUsers(),
                 taskDao.getNumberOfCreatedTasks(),
                 taskDao.getNumberOfCompletedTasks(),
                 subtaskDao.getNumberOfSubtasks(),
                 notificationDao.getNumberOfNotifications()
         );
+    }
+
+    public List<TasksCountForDateDto> getNumberOfTasks() {
+        Map<LocalDate, Long> groupedByDate =
+                taskDao.findAll().stream()
+                        .map(task -> task.getDeadline().toLocalDate())
+                        .sorted()
+                        .collect(toList())
+                        .stream()
+                        .collect(groupingBy(date -> date, counting()))
+                        .entrySet().stream()
+                        .sorted(comparingByKey())
+                        .collect(toMap(Entry::getKey, Entry::getValue, (oldValue, newValue) -> oldValue, HashMap::new));
+
+        Map<LocalDate, Long> counts = new TreeMap<>();
+        groupedByDate.keySet().forEach(localDate -> counts.put(localDate, groupedByDate.get(localDate)));
+
+        Map<LocalDate, Long> sortedByDate = counts.entrySet().stream()
+                .sorted(comparingByKey())
+                .collect(toMap(Entry::getKey, Entry::getValue, (oldValue, newValue) -> oldValue, HashMap::new));
+
+        List<TasksCountForDateDto> result = new ArrayList<>();
+        sortedByDate.forEach((date, count) -> result.add(new TasksCountForDateDto(date, count)));
+
+        return result;
     }
 }
